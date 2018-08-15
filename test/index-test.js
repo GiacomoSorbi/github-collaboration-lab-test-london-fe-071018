1971b917ae9f1e7b1e4f6bc7705319fca7c7cc73
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-15 09:51:07 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -23,8 +23,4 @@ describe('this lab', () => {
 
 describe('the local repository', () => {
 
-    it('has README.md as a tracked file', () => {
-      chai.assert.pathExists('./my-repository/.git/index',  'no files are being tracked.  Use "git add ." to track all files in this repo');
-    })
-
 })
