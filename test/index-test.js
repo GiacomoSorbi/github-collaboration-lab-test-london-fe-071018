601dd45d9e4d07691a74c18bc5721b86e4e2eb44
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-15 09:51:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -11,10 +11,6 @@ describe('this lab', () => {
       chai.assert.isDirectory('./my-repository',  'no folder name "my-repository" was found');
     })
 
-    it('has a valid git repository initialized for the my-repository folder', () => {
-      chai.assert.isDirectory('./my-repository/.git',  'no ".git" folder was found within "/my-repository, used "git init" to initialize');
-    })
-
     it('has a README.md file in the my-repository folder', () => {
       chai.assert.pathExists('./my-repository/README.md',  'no README.md file found within "/my-repository"');
     })
